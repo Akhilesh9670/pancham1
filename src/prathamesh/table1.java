9f685da91da3ccedea8f7e38dc5f8af509c85c72
Akhilesh9670 <37590618+Akhilesh9670@users.noreply.github.com>
2018-03-20 22:30:04 +0530

    Add files via upload

---
+++ src/prathamesh/table1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package prathamesh;
+
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author prathamesh chavan
+ */
+class table1 {
+
+    table1(DefaultTableModel model) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+}
